de32fc2011ae7c5368d6150ef38b6b7a3b8cbccd
Justin Thompson <justinthompson@Justins-MacBook-Pro.local>
2017-07-18 22:04:01 -0700

    okay. lots of ways to go here

--- EsvApiInterface/main.cpp
+++ EsvApiInterface/main.cpp
@@ -5,11 +5,40 @@
 //  Created by Justin Thompson on 7/18/17.
 //  Copyright © 2017 Justin Thompson. All rights reserved.
 //
+//  http://www.esvapi.org/api
+
 
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+string scripture = "";
+
+//#if defined(__APPLE__) && defined(__MACH__)
+//char buff[32768];
+//sprintf(buff, "echo \"%s\" | pbcopy", scripture.c_str());
+//system(buff);
+//#endif
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	
+	char scriptOut[32768];
+	
+	string httpAddress = "http://www.esvapi.org/v2/rest/";
+	string book = "Gen";
+	string chapter = "5";
+	
+	// just open it from online
+	sprintf(scriptOut, "open %spassageQuery?key=IP\\&passage=%s+%s\\&include-headings=false", httpAddress.c_str(), book.c_str(), chapter.c_str());
+	system(scriptOut);
+	
+	// curl it from online, save as .html, open it from here
+	sprintf(scriptOut, "echo \"$(curl %spassageQuery?key=IP\\&passage=%s+%s\\&include-headings=false\\&output-format=crossway-xml-1.0)\" > scripture.html && open scripture.html", httpAddress.c_str(), book.c_str(), chapter.c_str());
+	system(scriptOut);
+	
+	
 	return 0;
 }
